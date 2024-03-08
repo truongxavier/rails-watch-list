088f038aa4129cf5e4cdf93b9de4f3e91648c533
Xavier Truong <truong.xavier@gmail.com>
2024-03-08 15:55:11 +0100

    watch-list006

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,15 @@
+# frozen_string_literal: true
+
+# class ReviewsHelper
 module ReviewsHelper
+  def render_stars(rating)
+    full_stars = rating.floor
+    half_star = (rating - full_stars).round(1) == 0.5
+
+    stars = ''
+    full_stars.times { stars += '★' }
+    stars += '☆' if half_star
+
+    stars.html_safe
+  end
 end
